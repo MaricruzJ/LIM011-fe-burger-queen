a77d75da48db2123218716e13f5944733a3244d5
MaricruzJ <maricruzj.er@gmail.com>
2020-03-05 18:27:32 -0500

    create functions for get data by category

--- src/app/components/product-catalog/product-catalog.component.ts
+++ src/app/components/product-catalog/product-catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from 'src/app/services/firestore/firestore.service';
 
 @Component({
   selector: 'app-product-catalog',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCatalogComponent implements OnInit {
 
-  constructor() { }
+  public products = [];
+  public productsFilter = [];
+
+  constructor(private firestoreService: FirestoreService) { }
 
   ngOnInit(): void {
+    this.firestoreService.getProducts().subscribe((productsSnapshot) => {
+      this.products = [];
+      productsSnapshot.forEach((productData: any) => {
+        this.products.push({
+          id: productData.payload.doc.id,
+          data: productData.payload.doc.data()
+        });
+      });
+    });
   }
 
+  getClassic() {
+    this.productsFilter = this.products.filter((product) => product.data.category === 'classic');
+    console.log(this.productsFilter);
+  }
+
+  getDrinks() {
+    this.productsFilter = this.products.filter((product) => product.data.category === 'drinks');
+    console.log(this.productsFilter);
+  }
+
+  getPiqueos() {
+    this.productsFilter = this.products.filter((product) => product.data.category === 'piqueos');
+    console.log(this.productsFilter);
+  }
+
+  getExtras() {
+    this.productsFilter = this.products.filter((product) => product.data.category === 'extras');
+    console.log(this.productsFilter);
+  }
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FirestoreService } from '../../services/firestore/firestore.service';
 
 @Component({
@@ -7,19 +7,9 @@ import { FirestoreService } from '../../services/firestore/firestore.service';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-  public products = [];
+  @Input() products: any[];
 
-  constructor(private firestoreService: FirestoreService) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.firestoreService.getProducts().subscribe((productsSnapshot) => {
-      this.products = [];
-      productsSnapshot.forEach((productData: any) => {
-        this.products.push({
-          id: productData.payload.doc.id,
-          data: productData.payload.doc.data()
-        });
-      });
-    });
-  }
+  ngOnInit(): void { }
 }
