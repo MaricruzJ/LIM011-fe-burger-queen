15b3c0d491efb881265c80d0febd20d37c502d34
MaricruzJ <maricruzj.er@gmail.com>
2020-03-09 17:29:31 -0500

    add functions for change quantity of products

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  public quantity = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  add() {
+    this.quantity = this.quantity + 1;
+  }
+
+  subtract() {
+    this.quantity = this.quantity - 1;
   }
 
 }
