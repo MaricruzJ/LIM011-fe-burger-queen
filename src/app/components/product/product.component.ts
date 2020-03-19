5b0a4a2a7f031b3dbf5ad988672e98e211efa536
KellyRoca <rocarondinel.kelly@gmail.com>
2020-03-19 11:15:09 -0500

    test to remove duplicates

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { OrderService } from 'src/app/services/order/order.service';
 import { OrderItem } from 'src/app/interfaces/order-item';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-product',
@@ -20,7 +21,7 @@ export class ProductComponent implements OnInit {
 
   constructor(private orderService: OrderService) {
     this.orderService.currentOrder.subscribe(array => {
-      this.arrayOrder = array;      
+      this.arrayOrder = array;
     });
     this.priceExtras = 0;
   }
@@ -34,30 +35,124 @@ export class ProductComponent implements OnInit {
     });
   }
 
+  position = 0
+  changeModalValue = 0
   toggleModal = (id: string) => {
     if (id != null) {
       this.productSelected = this.products.find((product) => product.id === id);
-      this.productSelected.data.quantity+= this.productSelected.data.quantity
+      this.productSelected.data.quantity += this.productSelected.data.quantity
     }
 
     if (this.productSelected.data.popup === true) {
       this.showModal = !this.showModal;
+      this.changeModalValue = this.changeModalValue + 1
     }
-    // if(this.productSelected.){}
+    console.log(this.changeModalValue);
 
-      this.item = {
-      id: this.productSelected.id,
+
+    //AGREGAR IDENTIFICADOR AL ID DEL ITEM, CON NOMBRES DE LOS EXTRAS
+    let letra = []
+    this.arrExtras.forEach((elem) => {
+      console.log(elem.data.name);
+
+      if (elem.data.name !== '') {
+        letra.push(elem.data.name.slice(0, 1))
+        letra.sort();
+      }
+
+    })
+
+    //CREACION DEL ITEM
+    this.item = {
+      id: this.productSelected.id + letra,
       quantity: 1,
       product: this.productSelected.data.name,
       extra: this.arrExtras,
       amount: this.productSelected.data.price + this.priceExtras,
       priceUnit: this.productSelected.data.price + this.priceExtras,
     };
+
+
+    // ENCONTRAR LA POSICION DEL ITEM REPETIDO
+    this.position = this.arrayOrder.findIndex((element) => element.id === this.item.id)
+    console.log(this.position);
+
+
+    if (this.position !== -1 ) {
+      this.item.quantity += this.item.quantity
+      console.log(this.item.quantity);
+      this.arrayOrder[this.position].quantity = this.arrayOrder[this.position].quantity + 1;
+    } 
+
+
     if (this.showModal === false) {
       this.arrayOrder.push(this.item);
+      this.arrExtras = [];
+      this.priceExtras = 0;
+      this.changeModalValue = 0
     }
+
+    // (ele.id === this.item.id) elemem de finINdex
+    //   if(position !== -1){
+    //     this.arrayOrder[position].quantity = this.arrayOrder[position].quantity + 1;
+    // }else{
+    //   this.item.quantity = 1
+
+    // }
+    // if (this.showModal === false && position === -1) {
+    //     this.arrayOrder.push(this.item);
+    //     this.arrExtras = [];
+    //     this.priceExtras = 0;
+    //   }
+    //   console.log(position);
+
+    //   console.log(this.item.id);
+
+
+
+
+
+
+
+    // this.position = this.arrayOrder.findIndex((element)=> element === this.item)
+    // console.log(this.position);
+
+
+    // if(this.position !== -1){
+    //   this.item.quantity += this.item.quantity
+    //   console.log(this.item.quantity);
+    //   this.arrayOrder[this.position].quantity = this.arrayOrder[this.position].quantity + 1;
+    // } else{
+    //   if(this.showModal === false){
+    //     this.arrayOrder.push(this.item);
+    //   }
+    // }
+
+
+    // if (position === -1 && this.showModal === false) {
+    //   this.arrayOrder.push(this.item);
+    //   console.log();
+
+    // } else {
+    //   this.item.quantity += this.item.quantity
+    //   console.log(this.item.quantity);
+    //   this.arrayOrder[position].quantity = this.arrayOrder[position].quantity + 1;
+
+    // }
+
+
+    // if (position !== -1 && this.showModal === true ) {
+    //   this.item.quantity += this.item.quantity
+    //   console.log(this.item.quantity);
+    //   this.arrayOrder[position].quantity = this.arrayOrder[position].quantity + 1;
+    //   // this.item = {}
+
+    // } else if (this.showModal === false) {
+    //   // this.item.quantity += this.item.quantity
+    //   this.arrayOrder.push(this.item);
+    // }
+
     this.orderService.addProductToOrder(this.arrayOrder);
-    this.arrExtras = [];
-    this.priceExtras = 0;
+
   }
 }
